b21ef51c3dc8f5a923f4342ec8c011952c0a1e4d
rbattstuff <rbattstuff@gmail.com>
2026-02-05 11:14:07 -0600

    Update run.sh
    Change to my USB FTD Device

--- nx584/run.sh
+++ nx584/run.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/with-contenv bashio
-python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.30.1:10000 --config /usr/app/src/config.ini
+#python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --connect 192.168.30.1:10000 --config /usr/app/src/config.ini
+python3 /usr/bin/nx584_server --listen 0.0.0.0 --port 5007 --serial /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AH02BNE7-if00-port0 --baud 9600 --config /config/config.ini
